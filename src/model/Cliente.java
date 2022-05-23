01b89aaf7fc836a8d75481b2c9f2dd383edc6f30
andrelarrosa <andre.larrosa@outlook.com>
2022-05-23 20:17:58 -0300

    final

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public class Cliente {
@@ -33,34 +32,3 @@ public class Cliente {
 	
 	
 }
-=======
-package model;
-
-public class Cliente {
-
-	private String nome;
-	private String cpf;
-	private String email;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-}
->>>>>>> 7eebcc526fd356bd24733d93dca08964960bb7cd
--- src/model/MovimentacaoProduto.java
+++ src/model/MovimentacaoProduto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public class MovimentacaoProduto {
@@ -31,32 +30,3 @@ public class MovimentacaoProduto {
 		this.cliente = cliente;
 	}
 }
-=======
-package model;
-
-public class MovimentacaoProduto {
-
-	private int movimentacao;
-	private int produto;
-	private int cliente;
-	
-	public int getMovimentacao() {
-		return movimentacao;
-	}
-	public void setMovimentacao(int movimentacao) {
-		this.movimentacao = movimentacao;
-	}
-	public int getProduto() {
-		return produto;
-	}
-	public void setProduto(int produto) {
-		this.produto = produto;
-	}
-	public int getCliente() {
-		return cliente;
-	}
-	public void setCliente(int cliente) {
-		this.cliente = cliente;
-	}
-}
->>>>>>> 7eebcc526fd356bd24733d93dca08964960bb7cd
