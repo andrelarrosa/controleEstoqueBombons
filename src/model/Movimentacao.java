7eebcc526fd356bd24733d93dca08964960bb7cd
André Luiz <andre.larrosa@outlook.com>
2022-05-23 19:55:58 -0300

    Add files via upload

---
+++ src/model/Movimentacao.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Movimentacao {
+
+	private String descricao;
+	private String tipo;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
---
+++ src/model/MovimentacaoProduto.java
@@ -0,0 +1,27 @@
+package model;
+
+public class MovimentacaoProduto {
+
+	private int movimentacao;
+	private int produto;
+	private int cliente;
+	
+	public int getMovimentacao() {
+		return movimentacao;
+	}
+	public void setMovimentacao(int movimentacao) {
+		this.movimentacao = movimentacao;
+	}
+	public int getProduto() {
+		return produto;
+	}
+	public void setProduto(int produto) {
+		this.produto = produto;
+	}
+	public int getCliente() {
+		return cliente;
+	}
+	public void setCliente(int cliente) {
+		this.cliente = cliente;
+	}
+}
